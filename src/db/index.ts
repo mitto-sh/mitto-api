596e7628181a3823b13164fae9bbc7672873af8d
Diego Hinrichs <93550789+Diego-Hinrichs@users.noreply.github.com>
2026-08-07 15:17:40 -0400

    feat: drizzle db connection

---
+++ src/db/index.ts
@@ -0,0 +1,13 @@
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import { env } from '../config/env'
+import * as schema from './schema'
+
+const client = postgres(env.DATABASE_URL, {
+  max: 10,
+  idle_timeout: 20,
+  connect_timeout: 10,
+})
+
+export const db = drizzle(client, { schema })
+export type DB = typeof db
